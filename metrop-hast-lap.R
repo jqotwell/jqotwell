e22af617a8e04cd265f0b930c251fa068adcbc7f
jqotwell <155702343+jqotwell@users.noreply.github.com>
2024-01-04 22:43:21 -0500

    Add files via upload

---
+++ metrop-hast-lap.R
@@ -0,0 +1,31 @@
+library(stats)
+library(NormalLaplace)
+library(EnvStats)
+
+x<-c()
+
+xk<-5
+
+
+
+
+for(i in 1:10000){
+  
+  y<-rnl(1,xk,1)
+  y_prob<- dnorm(y,0,1)
+  x_prob<- dnorm(xk,0,1)
+  p<-y_prob/x_prob
+  if ((p>=1 )|| (runif(1,0,1)<=p)){
+    xk<-y
+    x<-rbind(x,y)
+    
+  }
+  else{
+    x<-rbind(x,xk)
+
+  }
+  
+  
+}
+x<-tail(x,5000)
+z<-gofTest(x)

271c52e6785bb64439f2b1290bec39d0cc187a73
jqotwell <155702343+jqotwell@users.noreply.github.com>
2024-01-04 22:39:28 -0500

    Add files via upload

---
+++ rejectoionnorm.R
@@ -0,0 +1,12 @@
+library(stats)
+library(NormalLaplace)
+u<-runif(1000,0,1)
+z1<-rnl(1000,0,1)
+z2<-c()
+
+for(i in 1:length(u)){
+  if(u[i]<dnorm(z1[i],0,1)/(1.53*dnl(z1[i],0,1))){
+    z2<-rbind(z2,z1[i])
+  }
+}
+print(length(z2))
